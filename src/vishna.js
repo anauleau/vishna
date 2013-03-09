014a8a5a4b3970d4c11f41317f2bccfba3455293
Andreas Nauleau <anauleau@gmail.com>
2013-03-09 15:20:25 -0800

    adds titles to bubbles and value to color according to timescale

--- src/vishna.js
+++ src/vishna.js
@@ -6,8 +6,8 @@ var vishna = (function() {
       news: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=kkd937tfu53qzuesmc68j99k&limit=30&callback=?"
   },
       thread = /^(item[?]id[=][0-9]+)/,               //regexp for HN thread posts
-      w = Math.max( $(window).width() * 0.85, 960 ),  //width
-      h = Math.max( $(window).height() * 0.85, 600 ), //height
+      w = $(window).width() * 0.85,  //width
+      h = $(window).height() * 0.85, //height
       m = 20,                                         //margin
       center = {                                      //gravity center
           x : ( w - m ) / 2,
@@ -71,59 +71,58 @@ var vishna = (function() {
   }
 //d is equal to the object that is returned by the ajax request
 //url is urls.news
-  function searchCallback(data) {
-    var movies = data.movies;
-      $.each(movies, function(index, movie) {
-        console.log(movie);
-      });
-  }
 //NEED TO EDIT --->>
   function load( url, callback ){
-    $.getJSON(url, function( data ) {
-      console.log(data);
-      posts = data.movies;
-      next = posts.pop();
+      $.getJSON(url, function( data ) {
+        console.log(data);
+        posts = data.movies;
+        next = posts.pop();
 
-      posts.map( function(d) {
-        var comments = parseInt( d.critics_consensus ),
-            score    = parseInt( d.ratings.critics_score ),
-            time     = d.release_dates.theater.split(" ");
+        posts.map( function(d) {
+          var comments = d.critics_consensus,
+              score    = parseInt( d.ratings.critics_score ),
+              time     = d.release_dates.theater.split(" ").toString(),
+              rank     = 1;
+              var format = d3.time.format("%Y-%m-%d");
+          d.comments = comments;
+          d.score = score;
+          d.time = format.parse(time);
+          console.log(d.time);
+          // time[0] * t[ time[1] ]; // number * factor
+          // console.log(d.time)
+          d.url = d.links.alternate;
+          return d;
+        });
 
-        d.comments = comments;
-        d.score = score;
-        d.time = time[0] * t[ time[1] ]; // number * factor
+        // Defining the scales
+        r = d3.scale.linear()
+          .domain([ d3.min(posts, function(d) { return d.score; }),
+                    d3.max(posts, function(d) { return d.score; }) ])
+          .range([ 10, 100 ])
+          .clamp(true);
 
-        if ( thread.test(d.url) ) {
-          Sd.url = d.links.alternate;
-        }
-        console.log(d);
-        return d;
+        z = d3.scale.linear()
+          .domain([ d3.min(posts, function(d) { return d.comments; }),
+                    d3.max(posts, function(d) { return d.comments; }) ])
+          .range([ '#ff7f0e', '#ff7f0e' ]);
+
+        o = d3.scale.linear()
+          .domain([ d3.min(posts, function(d) { return d.time; }),
+                    d3.max(posts, function(d) { return d.time; }) ])
+          .range([ 0.2, 1 ]);
+
+        g = function(d) { return -r(d) * r(d) / 2.5; };
+
+        callback();
       });
-
-      // Defining the scales
-      r = d3.scale.linear()
-        .domain([ d3.min(posts, function(d) { return d.score; }),
-                  d3.max(posts, function(d) { return d.score; }) ])
-        .range([ 10, 130 ])
-        .clamp(true);
-
-      z = d3.scale.linear()
-        .domain([ d3.min(posts, function(d) { return d.comments; }),
-                  d3.max(posts, function(d) { return d.comments; }) ])
-        .range([ '#ff7f0e', '#ff7f0e' ]);
-
-      o = d3.scale.linear()
-        .domain([ d3.min(posts, function(d) { return d.time; }),
-                  d3.max(posts, function(d) { return d.time; }) ])
-        .range([ 1, 0.2 ]);
-
-      g = function(d) { return -r(d) * r(d) / 2.5; };
-
-      callback();
-    });
   }
 //<<-----
 
+
+var moveText = function(d){
+    d.text.attr('x', d.x)
+          .attr('y', d.y)
+};
 
 function launch() {
 
@@ -145,8 +144,10 @@ function launch() {
   var node = circles
     .enter()
     .append("a")
-    .attr("xlink:href", function(d) { return d.url; })
-    .append("circle")
+    .append("circle").call(force.drag)
+    .on('mousedown.foo', function(d){d.click = true;})
+    .on('mouseup.foo', function(d){d.click = false;})
+    .on('mousemove.foo', function(d){if(d.click)moveText(d);})
     .attr("r", 0)
     .attr("cx", function(d) { return d.x; })
     .attr("cy", function(d) { return d.y; })
@@ -157,7 +158,11 @@ function launch() {
     .attr("title", function(d) { return d.title; })
     .style("opacity", function(d) { return o( d.time ); })
     .on("mouseover", function(d, i) { force.resume(); highlight( d, i, this ); })
-    .on("mouseout", function(d, i) { downlight( d, i, this ); });
+    .on("mouseout", function(d, i) { downlight( d, i, this ); }).each(function(d, i){
+    d.text = svg.append('text').attr('x', d.x)
+                        .attr('y', d.y)
+                        .text(d.title)
+    });
 
   d3.selectAll("circle")
     .transition()
@@ -175,9 +180,11 @@ function launch() {
         .friction(friction)
         .on("tick", function(e) {
           generator(e.alpha);
-          node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+          d = node
+            .attr("cx", function(d) { return d.x;})
+            .attr("cy", function(d) { return d.y; })
+            .data()
+            d.forEach(moveText);
         }).start();
     }
 
@@ -190,78 +197,79 @@ function launch() {
     }
   }
 
-  function legend() {
+function legend() {
 
-    var linearGradient = svg.append("defs")
-      .append("linearGradient")
-      .attr("id", "legendGradient")
-      .attr("x1", "0%")
-      .attr("y1", "0%")
-      .attr("x2", "0%")
-      .attr("y2", "100%")
-      .attr("spreadMethod", "pad");
+  var linearGradient = svg.append("defs")
+    .append("linearGradient")
+    .attr("id", "legendGradient")
+    .attr("x1", "0%")
+    .attr("y1", "0%")
+    .attr("x2", "0%")
+    .attr("y2", "100%")
+    .attr("spreadMethod", "pad");
 
-    linearGradient
-      .append("stop")
-      .attr("offset", "0%")
-      .attr("stop-color", "#ff7f0c")
-      .attr("stop-opacity", "0.1");
+  linearGradient
+    .append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "#ff7f0c")
+    .attr("stop-opacity", "0.1");
 
-    linearGradient
-      .append("stop")
-      .attr("offset", "100%")
-      .attr("stop-color", "#ff7f0c")
-      .attr("stop-opacity", "1");
+  linearGradient
+    .append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "#ff7f0c")
+    .attr("stop-opacity", "1");
 
-    var legend = svg.append("g")
-      .attr("id", "legend");
+  var legend = svg.append("g")
+    .attr("id", "legend");
 
-    legend
-      .append("rect")
-      .attr("x", "20")
-      .attr("y", "20")
-      .attr("width", "20")
-      .attr("height", "200")
-      .attr("style", "fill:url(#legendGradient);");
+  legend
+    .append("rect")
+    .attr("x", "20")
+    .attr("y", "20")
+    .attr("width", "20")
+    .attr("height", "200")
+    .attr("style", "fill:url(#legendGradient);");
 
-    legend
-      .append("text")
-      .attr("x", 45)
-      .attr("y", 30)
-      .text("Oldest");
+  legend
+    .append("text")
+    .attr("x", 45)
+    .attr("y", 30)
+    .text("Oldest");
 
-    legend
-      .append("text")
-      .attr("x", 45)
-      .attr("y", 220)
-      .text("Newest");
+  legend
+    .append("text")
+    .attr("x", 45)
+    .attr("y", 220)
+    .text("Newest");
 
-  }
+}
 
-  //appends url for hover state
-  function highlight( data, i, element ) {
-    d3.select( element ).attr( "stroke", "black" );
 
-    var description = data.description.split("|"),
-      content = '<span class=\"title\"><a href=\"' + data.url + '\">' + data.title + '</a></span><br/>' +
-                 description[0] + "<br/>" +
-                '<a href=\"http://news.ycombinator.com/item?id='+ data.item_id +'\">' + description[1] + '</a>';
+//appends url for hover state
+function highlight( data, i, element ) {
+  d3.select( element ).attr( "stroke", "black" );
+return
+  var description = data.description.split("|"),
+    content = '<span class=\"title\"><a href=\"' + data.url + '\">' + data.title + '</a></span><br/>' +
+               description[0] + "<br/>" +
+              '<a href=\"http://news.ycombinator.com/item?id='+ data.item_id +'\">' + description[1] + '</a>';
 
-    tooltip.showTooltip(content, d3.event);
-  }
+  tooltip.showTooltip(content, d3.event);
+}
 
-  function downlight( data, i, element ) {
-    d3.select(element).attr("stroke", function(d) { return d3.rgb( z( d.comments )).darker(); });
-  }
+function downlight( data, i, element ) {
+  d3.select(element).attr("stroke", function(d) { return d3.rgb( z( d.comments )).darker(); });
+}
 
-  //Register category selectors
-  $("a.category").on("click", function(e) { update( $(this).attr("value") ); });
+//Register category selectors
+$("a.category").on("click", function(e) { update( $(this).attr("value") ); });
 
-  return {
-    categories : ["news", "movies"],
-    init : init,
-    update : update
-  };
+return {
+  categories : ["news", "movies"],
+  init : init,
+  update : update
+};
 
 })();
 
